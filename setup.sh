d7bffe8bd62ace35c65f8af1c44c881616bedec4
darkrentin <darkrentin@gmail.com>
2025-11-09 13:22:58 +0100

    modif tree

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+sudo pacman -Syu
+sudo pacman -Syu --noconfirm --needed $(cat install/pkgs.txt)
+
+./install/i3lock-color-install.sh
+
+cp -a config/. ~/
+
+i3 reload
+
+dunstify "Finished !" " Mod + Enter : open a terminal\n Mod + d : open dmenu"
