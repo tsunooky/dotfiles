75950a9e53234d8a7e86a8ef2a629591e4cfae49
darkrentin <darkrentin@gmail.com>
2025-11-09 13:23:57 +0100

    rm old file

--- .config/scripts/get_polybar_font.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-RES="$(xrandr | awk '/ [0-9]+x[0-9]+ / {print $1; exit}')"
-
-if res == "1920x1080"; then
-    echo "11"
-else
-    echo "22"
-fi
--- .config/scripts/screenres.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-RES="$(xrandr | awk '/ [0-9]+x[0-9]+ / {print $1; exit}')"
-
-xrandr --output eDP-1 --mode $RES --rate 120
