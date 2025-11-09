b80504ff778d64b759f28ef32e02a5289b2e7839
darkrentin <darkrentin@gmail.com>
2025-11-09 13:23:57 +0100

    rm old file

--- .config/scripts/random_wallpaper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-BASE_DIR="$HOME/.wallpapers"
-
-RANDOM_IMAGE=$(find "$BASE_DIR" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" \) | shuf -n 1)
-
-~/.config/scripts/change_wallpaper.sh "$RANDOM_IMAGE"
