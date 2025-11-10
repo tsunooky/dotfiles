3b4ff82a366a95beef2fc8e9d44bd61bff912c69
tsunooky <adrien.coureau91@gmail.com>
2025-11-10 01:22:58 +0100

    upgraded setup script, organized packages list, updated TODO

--- install/i3lock-color-install.sh
+++ install/i3lock-color-install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+sudo pacman -S --noconfirm --needed autoconf cairo fontconfig gcc libev libjpeg-turbo libxinerama libxkbcommon-x11 libxrandr pam pkgconf xcb-util-image xcb-util-xrm
+
 git clone https://github.com/Raymo111/i3lock-color.git /tmp/i3lock-color
 
 cd /tmp/i3lock-color
