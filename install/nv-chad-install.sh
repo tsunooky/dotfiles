e70bb34a4d147070bc62b3d3637759dd06caf219
tsunooky <adrien.coureau91@gmail.com>
2025-11-12 16:56:49 +0100

    fixed packages and NV Chad install

--- install/nv-chad-install.sh
+++ install/nv-chad-install.sh
@@ -13,8 +13,9 @@ echo '
 
 os.execute("python ~/.config/nvim/pywal/chadwal.py &> /dev/null &")
 
-local autocmd = vim.api.nvim_create_autocmdautocmd("Signal", {
+local autocmd = vim.api.nvim_create_autocmd
 
+autocmd("Signal", {
   pattern = "SIGUSR1",
   callback = function()
     require("nvchad.utils").reload()
