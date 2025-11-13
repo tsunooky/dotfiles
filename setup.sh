b563a8adc7699a058c6260c978e0ef929f09015b
Darkrentin <darkrentin@gmail.com>
2025-11-13 15:16:55 +0100

    fix setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo pacman -Syu --no-confirm
+sudo pacman -Syu --noconfirm
 sudo systemctl enable rfkill-unblock@all
 sudo pacman -S --noconfirm --needed archlinux-keyring sed
 ./install/pacman.sh
@@ -9,17 +9,15 @@ sudo pacman -S --noconfirm --needed $(cat install/pkgs.txt)
 
 ./install/i3lock-color-install.sh
 ./install/yay-install.sh
-./install/pywal-fox-install.sh
 ./install/nv-chad-install.sh
+./install/pywal-fox-install.sh
 yay -S --noconfirm --answerdiff None --answerclean None matugen-bin
-
 
 cp -a config/. ~/
 
 ./install/vim-install.sh
 
 export GTK_THEME="Adwaita:dark"
-i3 reload
 fc-cache -f
 
 sudo systemctl enable NetworkManager.service
@@ -27,12 +25,10 @@ sudo systemctl start NetworkManager.service
 sudo systemctl enable wpa_supplicant.service
 sudo systemctl start wpa_supplicant.service
 
-systemctl --user enable --now pipewire.socket
-systemctl --user enable --now wireplumber.service
+systemctl --user enable pipewire-pulse.service
+systemctl --user enable wireplumber.service
 
-sudo systemctl enable --now ly.service
-
-#alacritty & disown && exit
+sudo systemctl enable ly.service
 
 ./install/user-preferences.sh
 
