a951c951ffd13cef235f77f0bc60360b01adb9fc
tsunooky <adrien.coureau91@gmail.com>
2025-11-15 21:03:32 +0100

    added acpid package for laptops

--- install/laptop.sh
+++ install/laptop.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+# TLP
 sudo pacman -S --noconfirm --needed tlp tlp-rdw
 sudo systemctl enable --now tlp.service
 sudo systemctl enable NetworkManager-dispatcher.service
 sudo systemctl mask systemd-rfkill.service systemd-rfkill.socket
+
+# acpid
+sudo pacman -S --noconfirm --needed acpid
+sudo systemctl enable --now acpid.service
