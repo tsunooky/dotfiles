aee3d6fcd67d0e37c8fd2970924546df2e8d43ed
tsunooky <adrien.coureau91@gmail.com>
2025-11-17 17:02:56 +0100

    fixed vim install 2

--- install/vim-install.sh
+++ install/vim-install.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+sudo pacman -S --noconfirm --needed vim
+
+mkdir -p ~/.vim/bundle
+
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+else
+  echo "Vundle is already cloned."
+fi
 
 vim +PluginInstall +qall
