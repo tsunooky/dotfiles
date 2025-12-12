6d34e2dd497b750dd58b4cbaa2151c69b48d0936
darkrentin <darkrentin@gmail.com>
2025-12-12 11:27:47 +0100

    update i3lock

--- config/.config/scripts/lock.sh
+++ config/.config/scripts/lock.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 blank="00000000"
-circle_bg=#0c305f
-circle=#abc7ff
+circle_bg=#00344f
+circle=#96cdf8
 error=#ffb4ab
 
+i3-msg workspace number 42
+
 i3lock -c $blank \
-    --indicator \
     --inside-color=$blank \
     --insidever-color=$blank \
     --insidewrong-color=$blank \
@@ -19,3 +20,7 @@ i3lock -c $blank \
     --verif-color=$circle \
     --wrong-color=$error \
     --modif-color=$circle_bg \
+
+
+i3-msg workspace number 1
+
