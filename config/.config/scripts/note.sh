9973469f51435664fd82ea5957acc4a40dad4190
darkrentin <darkrentin@gmail.com>
2025-12-12 11:18:16 +0100

    fix note book

--- config/.config/scripts/note.sh
+++ config/.config/scripts/note.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-if [ ! -f "$HOME/.note.lock" ] || ! (kill -s 0 $(cat ~/.note.lock));  then
-    clsw
-    touch ~/.note.lock
-    echo $PPID > ~/.note.lock
-    vim ~/.note.txt +startinsert
-    rm ~/.note.lock
+if [ -f "$HOME/.note.lock" ] && (kill -s 0 $(cat ~/.note.lock));  then
+    i3-msg '[title="notebook"] focus'
+    exit 0
 fi
+clsw
+touch ~/.note.lock
+echo $PPID > ~/.note.lock
+echo -ne "\033]0;notebook\007"
+vim ~/.note.txt +startinsert
+rm ~/.note.lock
 
